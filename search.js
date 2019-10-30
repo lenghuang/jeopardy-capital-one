7fa60592c23504e49ca7560231d2e8207655afc2
Len Huang <32604574+lenghuang@users.noreply.github.com>
2019-10-29 20:38:48 -0400

    adapted for different url

--- search.js
+++ search.js
@@ -4,8 +4,8 @@ var clueContainer = document.getElementById("clue-info");
 btn.addEventListener("click", function(){
     var ourData;
     var ourRequest = new XMLHttpRequest();
-    var offset = 0;
-    ourRequest.open("GET","http://jservice.io/api/clues?=" + offset);
+    var id = 1;
+    ourRequest.open("GET","http://jservice.io/api/category?id=" + id);
     // Function to load text as json object
     ourRequest.onload = function() {
         ourData = JSON.parse(ourRequest.responseText);
@@ -17,9 +17,9 @@ btn.addEventListener("click", function(){
 
 // Function to write gathered data out to HTML
 function renderHTML(data){
-    var htmlString = "";
-    for (i = 0; i <data.length; i++){
-        htmlString += "<p>" + data[i]["question"] + " " + data[i]["answer"] + "</p>";
+    var htmlString = "<h1> Category: " + data["title"] + "</h1>";
+    for (i = 0; i < data["clues"].length; i++){
+        htmlString += "<p>" + data["clues"][i]["question"] + " " + data["clues"][i]["answer"] + "</p>";
     }
     clueContainer.insertAdjacentHTML("beforeend",htmlString);
 }
